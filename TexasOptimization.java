fd34f7dc2303d61ab4eb288b19f62fb6404c2d8f
jemminiz <jonathan.emminizer@yahoo.com>
2023-09-13 10:05:03 -0400

    Added program
    File error, added the code for original commit

---
+++ TexasOptimization.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class TexasOptimization
+{
+    private final int BUBBA_SAVAGERY = 40;
+    public int BubSav(int currSav)
+    {
+        for(int i = 0; i < 50; i++)
+        {
+            if(currSav < BUBBA_SAVAGERY)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+    public int Savagery(String famMem, int currSav)
+    {
+        
+
+        return 1;
+    }
+    public int GatheringHit(String famMem, int currGath)
+    {
+        
+
+        return 1;
+    }
+    public int GatheringExec(String famMem, int currGath)
+    {
+        
+
+        return 1;
+    }
+    public int GatheringBucket(String famMem, int currGath)
+    {
+        
+
+        return 1;
+    }
+    public int GatheringEncounter(String famMem, int currGath)
+    {
+        
+
+        return 1;
+    }
+}
