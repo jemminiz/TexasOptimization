c155b1457590efad8baaff75713a3d4fe069d789
jemminiz <72472839+jemminiz@users.noreply.github.com>
2023-10-12 08:22:37 -0400

    Button should now print to the screen instead of alerting

--- TexasOptimizationCalc.js
+++ TexasOptimizationCalc.js
@@ -1,15 +1,18 @@
 function calcSav(char, sav, tough)
 {
+    var item = document.getElementById("text");
     if(char == "Leatherface")
     {
-        return (tough * 1.11 + 83.25) / (20 + (2.0/5 * sav - 10));
+        item.innerHTML = "It would take " + (tough * 1.11 + 83.25) / (20 + (2.0/5 * sav - 10)) + " to kill someone with " + tough + " toughness at " + sav + " savagery on Leatherface.";
     }
     else if(char == "Cook")
     {
-        return (tough * 1.11 + 83.25) / (16 + (2.0/5 * sav - 10));
+        item.innerHTML = "It would take " + (tough * 1.11 + 83.25) / (16 + (2.0/5 * sav - 10)) + " to kill someone with " + tough + " toughness at " + sav + " savagery on Cook.";
     }
     else if(char == "Sissy" || char == "Hitchhiker")
-        return (tough * 1.11 + 83.25) / (16 + (8.0/25 * sav - 8));
+        item.innerHTML = "It would take " + (tough * 1.11 + 83.25) / (16 + (8.0/25 * sav - 8)) + " to kill someone with " + tough + " toughness at " + sav + " savagery on Sissy.";
+    else if(char == "Hitchhiker")
+        item.innerHTML = "It would take " + (tough * 1.11 + 83.25) / (16 + (8.0/25 * sav - 8)) + " to kill someone with " + tough + " toughness at " + sav + " savagery on Hitchhiker.";
     else
-        return (tough * 1.11 + 83.25) / (18 + (8.0/25 * sav - 8));
+        item.innerHTML = "It would take " + (tough * 1.11 + 83.25) / (18 + (8.0/25 * sav - 8)) + " to kill someone with " + tough + " toughness at " + sav + " savagery on Johnny.";
 }
